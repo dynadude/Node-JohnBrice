c4ccba7abad466668d02c47b5493577688cfad51
Sagi gamliel <sagi.gam.11@gmail.com>
2023-03-23 10:09:05 +0200

    changed http-basic to have multiple rpites

--- http-basic.js
+++ http-basic.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,10 +15,39 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const HOST = 'localhost';
 const PORT = '8080';
+//function JSONArrayToCSV(array: Array<Object>) {
+//    let csv: string = "";
+//    let firstKeys = Object.keys(array[0]);
+//
+//    csv += firstKeys.join(', ');
+//
+//    for (let i = 0; i < array.length; i++) {
+//	let keys = Object.keys(array[i])
+//
+//	for (let key in array[i]) {
+//	    if (array[i][key]) {
+//	    }
+//	    console.log(array[i][key]);
+//	}
+//    }
+//}
 const server = http_1.default.createServer(function (req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(JSON.stringify({ Name: "Shai Manor" }, null, 3));
+    return __awaiter(this, void 0, void 0, function* () {
+        let method = req.method;
+        let url = req.url;
+        if (method == "GET" && url == '/age') {
+            res.writeHead(200);
+            res.end("20");
+            return;
+        }
+        if (method == "POST" && url == '/name') {
+            res.writeHead(200);
+            res.end("sagi");
+            return;
+        }
+        res.writeHead(404);
+        res.end("Kama Od");
+    });
 });
 server.listen(PORT, () => {
     console.log(`Server is running on http://${HOST}:${PORT}`);
--- http-basic.ts
+++ http-basic.ts
@@ -3,10 +3,42 @@ import http from 'http';
 const HOST = 'localhost';
 const PORT = '8080';
 
-const server = http.createServer(function (req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(JSON.stringify({Name: "Shai Manor"}, null, 3));
+//function JSONArrayToCSV(array: Array<Object>) {
+//    let csv: string = "";
+//    let firstKeys = Object.keys(array[0]);
+//
+//    csv += firstKeys.join(', ');
+//
+//    for (let i = 0; i < array.length; i++) {
+//	let keys = Object.keys(array[i])
+//
+//	for (let key in array[i]) {
+//	    if (array[i][key]) {
+//	    }
+//	    console.log(array[i][key]);
+//	}
+//    }
+//}
+
+const server = http.createServer(async function (req, res) {
+
+    let method = req.method;
+    let url = req.url;
+
+    if (method == "GET" && url == '/age') {
+	res.writeHead(200);
+	res.end("20");
+	return;
+    }
+
+    if (method == "POST" && url == '/name') {
+	res.writeHead(200);
+	res.end("sagi");
+	return;
+    }
+
+	res.writeHead(404);
+	res.end("Kama Od");
 });
 
 server.listen(PORT, () => {
